bb0018e344ee831ccd9d380bd7950dd2ec6dc1d0
Aaron Chew <aaronchewbani@gmail.com>
2022-11-08 17:31:37 -0800

    dapp

--- web-dapp/pages/api/initial.js
+++ web-dapp/pages/api/initial.js
@@ -20,3 +20,35 @@ let input={
         },
     },
 };
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log("Result : ", output);
+
+ABI = output.contracts["initial.sol"]["initial"].abi;
+bytecode = output.contracts["initial.sol"]["initial"].evm.bytecode.object;
+console.log("Bytecode: ", bytecode);
+console.log("ABI: ", ABI);
+
+contract = new web3.eth.Contract(ABI);
+web3.eth.getAccounts().then((accounts) => {
+    // Display all Ganache Accounts
+    console.log("Accounts:", accounts);
+
+    mainAccount = accounts[0];
+
+    // address that will deploy smart contract
+    console.log("Default Account:", mainAccount);
+    contract
+        .deploy({ data: bytecode })
+        .send({ from: mainAccount, gas: 470000 })
+        .on("receipt", (receipt) => {
+
+            // Contract Address will be returned here
+            console.log("Contract Address:", receipt.contractAddress);
+        })
+        .then((initialContract) => {
+            initialContract.methods.message().call((err, data) => {
+                console.log("Initial Data:", data);
+            });
+        });
+});
